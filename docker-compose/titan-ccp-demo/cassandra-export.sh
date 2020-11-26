002779c61dcc1ebf1d116c955995dcf12c5f9382
Sören Henning <soeren.henning@email.uni-kiel.de>
2020-11-26 11:53:15 +0100

    Add import/export scripts

---
+++ docker-compose/titan-ccp-demo/cassandra-export.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Export Cassandra schema
+docker-compose exec cassandra cqlsh -e "DESC KEYSPACE titanccp" > schema.cql
+
+# Export Cassandra tables
+docker-compose exec cassandra cqlsh -k titanccp -e "COPY activepowerrecord TO STDOUT;" > activepowerrecord.csv
+docker-compose exec cassandra cqlsh -k titanccp -e "COPY aggregatedactivepowerrecord TO STDOUT;" > aggregatedactivepowerrecord.csv
+docker-compose exec cassandra cqlsh -k titanccp -e "COPY onedayaggregation TO STDOUT;" > onedayaggregation.csv
+docker-compose exec cassandra cqlsh -k titanccp -e "COPY onehouraggregation TO STDOUT;" > onehouraggregation.csv
+docker-compose exec cassandra cqlsh -k titanccp -e "COPY hourofdayactivepowerrecord TO STDOUT;" > hourofdayactivepowerrecord.csv
+docker-compose exec cassandra cqlsh -k titanccp -e "COPY hourofweekactivepowerrecord TO STDOUT;" > hourofweekactivepowerrecord.csv
+docker-compose exec cassandra cqlsh -k titanccp -e "COPY dayofweekactivepowerrecord TO STDOUT;" > dayofweekactivepowerrecord.csv
---
+++ docker-compose/titan-ccp-demo/cassandra-import.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Import Cassandra schema
+docker-compose exec -T cassandra cqlsh -e "$(cat schema.cql)"
+
+# Import Cassandra data
+cat activepowerrecord.csv | docker-compose exec -T cassandra cqlsh -k titanccp -e "COPY activepowerrecord FROM STDIN WITH INGESTRATE=50000;"
+cat aggregatedactivepowerrecord.csv | docker-compose exec -T cassandra cqlsh -k titanccp -e "COPY aggregatedactivepowerrecord FROM STDIN WITH INGESTRATE=50000;"
+cat dayofweekactivepowerrecord.csv | docker-compose exec -T cassandra cqlsh -k titanccp -e "COPY dayofweekactivepowerrecord FROM STDIN WITH INGESTRATE=50000;"
+cat hourofdayactivepowerrecord.csv | docker-compose exec -T cassandra cqlsh -k titanccp -e "COPY hourofdayactivepowerrecord FROM STDIN WITH INGESTRATE=50000;"
+cat hourofweekactivepowerrecord.csv | docker-compose exec -T cassandra cqlsh -k titanccp -e "COPY hourofweekactivepowerrecord FROM STDIN WITH INGESTRATE=50000;"
+cat onedayaggregation.csv | docker-compose exec -T cassandra cqlsh -k titanccp -e "COPY onedayaggregation FROM STDIN WITH INGESTRATE=50000;"
+cat onehouraggregation.csv | docker-compose exec -T cassandra cqlsh -k titanccp -e "COPY onehouraggregation FROM STDIN WITH INGESTRATE=50000;"
